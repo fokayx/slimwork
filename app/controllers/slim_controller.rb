31e33febd04c2756b98c630a08afae90cca34354
fokayx <fokayx@gmail.com>
2015-03-31 09:13:35 +0800

    slim template initialize

---
+++ app/controllers/slim_controller.rb
@@ -0,0 +1,6 @@
+class SlimController < ApplicationController
+
+  def index
+  end
+
+end
---
+++ test/controllers/slim_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SlimControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
